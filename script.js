3c9a4d241a19cd863d3c3142d2eb57556902c0cc
Caleb Clapper <104874097+Caleb518c@users.noreply.github.com>
2023-03-22 19:04:46 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,77 @@
-//Sets the checkboxs on by default
-document.getElementById("cityCheckbox").checked = true;
-document.getElementById("townCheckbox").checked = true;
-document.getElementById("routeCheckbox").checked = true;
-document.getElementById("caveCheckbox").checked = true;
-document.getElementById("mansionCheckbox").checked = true;
-document.getElementById("shrineCheckbox").checked = true;
-document.getElementById("ruinCheckbox").checked = true;
-document.getElementById("forestCheckbox").checked = true;
-
-
-var aboutText = "Interactive map for Octopath Traveler 2.";
-
-function addAndRemoveParagraph() {
-    const aboutListItem = document.querySelector(".sideNav li#about"); // Get a reference to the "About" li element
-    const p = document.createElement("p"); // Create a new p element
-    p.textContent = aboutText; // Add some text to it
+    //Sets all checkboxes on by default.
+    document.getElementById("cityCheckbox").checked = true;
+    document.getElementById("townCheckbox").checked = true;
+    document.getElementById("routeCheckbox").checked = true;
+    document.getElementById("caveCheckbox").checked = true;
+    document.getElementById("mansionCheckbox").checked = true;
+    document.getElementById("shrineCheckbox").checked = true;
+    document.getElementById("ruinCheckbox").checked = true;
+    document.getElementById("forestCheckbox").checked = true;
     
-    const sideNav = document.getElementsByClassName("sideNav")[0]; // Get a reference to the sideNav element
-    
-    if (sideNav.querySelector("#aboutText")) {
-      // If the "aboutText" element exists, remove it
-      sideNav.querySelector("#aboutText").remove();
-    } else {
-      // Insert the p element after the "About" li element
-      aboutListItem.parentNode.insertBefore(p, aboutListItem.nextSibling);
-      p.setAttribute("id", "aboutText");
+
+    //Hides all expandable paragraphs on load.
+    document.querySelector("#aboutText").style.display = "none";
+
+
+    //Toggles about section paragraph.
+    var aboutHeading = document.getElementById("about");
+    var aboutParagraph = document.getElementById("aboutText");
+
+    aboutHeading.addEventListener("click", function(){
+        if (aboutParagraph.style.display == "flex"){
+            aboutParagraph.style.display = "none";
+        }
+        else{
+            aboutParagraph.style.display = "flex";
+        }
+    })
+
+
+
+
+    //Creates the map object in leaflet, then sets the image and bound for it.
+    var mapOptions = {
+        crs: L.CRS.Simple, 
+        minZoom: -1.75,
+        maxZoom: 1
     }
-}
+    var map = L.map('map', mapOptions);
+    var bounds = [[0,0], [3168, 5632]];
+    var image = L.imageOverlay('Media/Octopath II Map High Res-min(1)(2).jpg', bounds).addTo(map);
+    map.fitBounds(bounds);
+    
+    
 
-var sideNav = document.getElementById("sideNav");
+
+
+
+
 var menuFlag = true;
-function showMenu(){
+document.querySelector("#menuIcon").addEventListener("click", function(){
     if (menuFlag){
-        sideNav.classList.add("sideNavDisplay");
+        (document.querySelector("#sideNav")).style.display = "flex";
         menuFlag = false;
+        console.log(1);
     }
     else{
-        sideNav.classList.remove("sideNavDisplay");
+        document.querySelector("#sideNav").style.display = "none";
         menuFlag = true;
+        console.log(2);
     }
-
-}
-
+})
 
 
 
-var mapOptions = {
-    crs: L.CRS.Simple, 
-    minZoom: -1.75,
-    maxZoom: 1
-}
-  
-var map = L.map('map', mapOptions);
 
-var bounds = [[0,0], [3168, 5632]];
-var image = L.imageOverlay('Media/Octopath II Map High Res-min(1)(2).jpg', bounds).addTo(map);
-map.fitBounds(bounds);
 
-  
-const iconScale = 113;
+
+
+
 
 /*-------Icons------*/
 {
+const iconScale = 113;
+
 var townIcon = L.icon({
     iconUrl: 'Media/Icons/town.png', 
     iconSize: [iconScale/2.2, iconScale/2.8]
@@ -96,15 +106,30 @@ var forestIcon = L.icon({
 });
 }
 
-
-
 /*------Hineouma Region-------*/ 
 {
 var ku = L.marker([600, 590], {icon: cityIcon});
 ku.bindPopup("<b>Hinoeuma</b><br>City of Ku.");
 
 var sai = L.marker([855, 1166], {icon: townIcon});
-sai.bindPopup("<b>Hinoeuma</b><br>Town of Sai.");
+sai.bindPopup("");
+
+sai.on('mouseover', function(e){
+    this.sai = sai;
+    this.sai.bindPopup("<b>Hinoeuma</b><br>Town of Sai.");
+    this.sai.openPopup();
+})
+sai.on('click', function(e){
+    sai.bindPopup("<b>Hinoeuma</b><br>Town of Sai.<br><br> This is were the more detailed text will go.");
+    sai.openPopup();
+})
+
+ku.on('mouseover', function(e){
+    ku.openPopup();
+})
+
+
+
 
 
 //---------------Rename these later once I learn the real names------------------
@@ -125,8 +150,8 @@ var desertShrine = L.marker([635, 1400], {icon: shrineIcon});
 var mansionNorthOfShrine = L.marker([830, 1455], {icon: mansionIcon});
 var mansionNorthOfKu = L.marker([650, 670], {icon: mansionIcon});
 //Change this to house icon once I get one
-var houseInFarNorth = L.marker([1200, 1470], {icon: townIcon});
-var farNorthRoute = L.marker([1330, 1518], {icon: routeIcon});
+var ryu = L.marker([1200, 1470], {icon: townIcon});
+var northernRyuSands = L.marker([1330, 1518], {icon: routeIcon});
 }
 
 /*------Western Forest Region-------*/
@@ -155,94 +180,103 @@ var farNorthRoute = L.marker([1330, 1518], {icon: routeIcon});
 }
 
 
-
-
-var cities = L.layerGroup([ku, forestCity]);
-cities.addTo(map);
-
-
-var towns = L.layerGroup([sai, forestNorthTown, forestNorthEastHouse, houseInFarNorth]);
-towns.addTo(map);
-
-var routes = L.layerGroup([routeEastofForest, routeFarWestofSai, 
-    routeNorthOfDesertShrine, routeNorthWestofSai, routeOnBorder,
-    routeSouthEastOfKu, routeSouthOfKu, routeSouthofSai, 
-    routeWestofSai, forestRouteToMansion, routeEastOfForestCity,
-    routeNorthEastOfForestCity, routeNorthOfForestCave, routeNorthOfForestCave2,
-    routeNorthOfForestCave3, routeNorthOfForestCave4, farNorthRoute]);
-routes.addTo(map);
-
-var caves = L.layerGroup([caveEastOfSai, caveNorthOfKu, caveNorthOfSai,
-    southForestCave]);
-caves.addTo(map);
-
-var mansions = L.layerGroup([mansionNorthOfKu, mansionNorthOfShrine, northForestMansion, 
-    southForestMansion]);
-mansions.addTo(map);
-
-var shrines = L.layerGroup([desertShrine, forestShrine]);
-shrines.addTo(map);
-
-var ruins = L.layerGroup([ruinsEastOfKu]);
-ruins.addTo(map);
-
-var forests = L.layerGroup([southForest, northForst, veryNorthForest, 
-    veryNorthForestButWest]);
-forests.addTo(map);
-
-
-
-
-function toggleCities(){
-    if (document.getElementById("cityCheckbox").checked){
-        cities.addTo(map);
-    }
-    else { cities.remove(); }
-}
-function toggleTowns(){
-    if (document.getElementById("townCheckbox").checked){
-        towns.addTo(map);
-    }
-    else { towns.remove(); }
-}
-function toggleRoutes(){
-    if (document.getElementById("routeCheckbox").checked){
-        routes.addTo(map);
-    }
-    else { routes.remove(); }
-}
-function toggleCaves(){
-    if (document.getElementById("caveCheckbox").checked){
-        caves.addTo(map);
-    }
-    else { caves.remove(); }
-}
-function toggleMansions(){
-    if (document.getElementById("mansionCheckbox").checked){
-        mansions.addTo(map);
-    }
-    else { mansions.remove(); }
-}
-function toggleShrines(){
-    if (document.getElementById("shrineCheckbox").checked){
-        shrines.addTo(map);
-    }
-    else { shrines.remove(); }
-}
-function toggleRuins(){
-    if (document.getElementById("ruinCheckbox").checked){
-        ruins.addTo(map);
-    }
-    else { ruins.remove(); }
-}
-function toggleForests(){
-    if (document.getElementById("forestCheckbox").checked){
-        forests.addTo(map);
-    }
-    else { forests.remove(); }
+//Adds all layers to map
+{
+    var cities = L.layerGroup([ku, forestCity]);
+    cities.addTo(map);
+    
+    var towns = L.layerGroup([sai, forestNorthTown, forestNorthEastHouse, ryu]);
+    towns.addTo(map);
+    
+    var routes = L.layerGroup([routeEastofForest, routeFarWestofSai, 
+        routeNorthOfDesertShrine, routeNorthWestofSai, routeOnBorder,
+        routeSouthEastOfKu, routeSouthOfKu, routeSouthofSai, 
+        routeWestofSai, forestRouteToMansion, routeEastOfForestCity,
+        routeNorthEastOfForestCity, routeNorthOfForestCave, routeNorthOfForestCave2,
+        routeNorthOfForestCave3, routeNorthOfForestCave4, northernRyuSands]);
+    routes.addTo(map);
+    
+    var caves = L.layerGroup([caveEastOfSai, caveNorthOfKu, caveNorthOfSai,
+        southForestCave]);
+    caves.addTo(map);
+    
+    var mansions = L.layerGroup([mansionNorthOfKu, mansionNorthOfShrine, northForestMansion, 
+        southForestMansion]);
+    mansions.addTo(map);
+    
+    var shrines = L.layerGroup([desertShrine, forestShrine]);
+    shrines.addTo(map);
+    
+    var ruins = L.layerGroup([ruinsEastOfKu]);
+    ruins.addTo(map);
+    
+    var forests = L.layerGroup([southForest, northForst, veryNorthForest, 
+        veryNorthForestButWest]);
+    forests.addTo(map); 
 }
 
+//Toggles layers on the map when the checkboxes are clicked.
+{
+$("#cityCheckbox").click(function(){
+    if ($("#cityCheckbox").prop('checked')) 
+        cities.addTo(map); 
+    else 
+        cities.remove(); 
+})
+
+$("#townCheckbox").click(function(){
+    if ($("#townCheckbox").prop('checked')) 
+        towns.addTo(map); 
+    else 
+        towns.remove(); 
+})
+
+$("#routeCheckbox").click(function(){
+    if ($("#routeCheckbox").prop('checked')) 
+        routes.addTo(map); 
+    else 
+        routes.remove(); 
+})
+
+$("#caveCheckbox").click(function(){
+    if ($("#caveCheckbox").prop('checked')) 
+        caves.addTo(map); 
+    else 
+        caves.remove(); 
+})
+
+$("#mansionCheckbox").click(function(){
+    if ($("#mansionCheckbox").prop('checked')) 
+        mansions.addTo(map); 
+    else 
+        mansions.remove(); 
+})
+
+$("#shrineCheckbox").click(function(){
+    if ($("#shrineCheckbox").prop('checked')) 
+        shrines.addTo(map); 
+    else 
+        shrines.remove(); 
+})
+
+$("#ruinCheckbox").click(function(){
+    if ($("#ruinCheckbox").prop('checked')) 
+        ruins.addTo(map); 
+    else 
+        ruins.remove(); 
+})
+
+$("#forestCheckbox").click(function(){
+    if ($("#forestCheckbox").prop('checked')) 
+        forests.addTo(map); 
+    else 
+        forests.remove(); 
+})
+}
 
 
 
 
+
+
+  
